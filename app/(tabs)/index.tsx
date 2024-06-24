06f4d547fa76beba44c43a592039a9499b6a0b00
Toufiquer <toufiquerabc@gmail.com>
2024-06-25 00:40:50 +0600

    backup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,21 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useGlobalStore } from "@/components/store";
+import { useEffect } from "react";
 
 export default function TabOneScreen() {
+  const setApiData = useGlobalStore((store) => store.setApiData);
+
+  useEffect(() => {
+    fetch(process.env.EXPO_PUBLIC_API_URL as string)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+    // .then((data) => console.log("data : ", data));
+  }, []);
   return (
     <View style={styles.container}>
-      <Text className="text-rose-400">Custom text</Text>
+      <Text className="text-rose-400 text-2xl">Custom text</Text>
       <Text style={styles.title}>Tab One</Text>
       <View
         style={styles.separator}
---
+++ components/store.ts
@@ -0,0 +1,90 @@
+/*
+|-----------------------------------------
+| setting up Store for the App
+| @author: Toufiquer Rahman<toufiquer.0@gmail.com>
+| @copyright: Toufiquer, June, 2024
+|-----------------------------------------
+*/
+
+import { create } from "zustand";
+
+export type initRenderOnboardingStatusType = {
+  currentBoard: string;
+  newBoard: boolean;
+  isRender: boolean;
+  isUpdate: boolean;
+  isAddBoard: boolean;
+  isUpdateBoard: boolean;
+  isDeleteBoard: boolean;
+  isAddTask: boolean;
+  isUpdateTask: boolean;
+  isDeleteTask: boolean;
+  isViewTask: boolean;
+  currentTitle: string;
+  currentDescription: string;
+  currentId: string;
+};
+export const initRenderOnboardingStatus: initRenderOnboardingStatusType = {
+  currentBoard: "",
+  newBoard: false,
+  isRender: false,
+  isUpdate: false,
+  isAddBoard: false,
+  isUpdateBoard: false,
+  isDeleteBoard: false,
+  isAddTask: false,
+  isUpdateTask: false,
+  isDeleteTask: false,
+  isViewTask: false,
+  currentTitle: "",
+  currentDescription: "",
+  currentId: "",
+};
+interface useGlobalStoreType {
+  isRestaurant: boolean;
+  setIsRestaurant: (payload: any) => void;
+  currentItem: string;
+  setCurrentItem: (payload: any) => void;
+  apiData: any;
+  setApiData: (payload: any) => void;
+  mutationData: any;
+  setMutationData: (payload: any) => void;
+  dashboardData: any;
+  setDashboardData: (payload: any) => void;
+  boardTask: {
+    data: {
+      id: string;
+      title: string;
+      description?: string | undefined;
+      status?: string;
+      lstUpdate: Date | any;
+    }[];
+    statusLst: { id: string; title: string; description?: string }[];
+  };
+  setBoardTask: (payload: any) => void;
+  onBoardingStatus: initRenderOnboardingStatusType;
+  setonBoardingStatus: (payload: any) => void;
+}
+export const useGlobalStore = create<useGlobalStoreType>((set) => ({
+  isRestaurant: true,
+  setIsRestaurant: (payload) => set(() => ({ isRestaurant: payload })),
+  currentItem: "",
+  setCurrentItem: (payload) => set(() => ({ currentItem: payload })),
+  apiData: [],
+  setApiData: (payload) => set(() => ({ apiData: payload })),
+  mutationData: {},
+  setMutationData: (payload) => set(() => ({ mutationData: payload })),
+  dashboardData: {},
+  setDashboardData: (payload) => set(() => ({ dashboardData: payload })),
+  boardTask: {
+    data: [],
+    statusLst: [
+      { id: "1", title: "Task" },
+      { id: "2", title: "Inprogress" },
+      { id: "3", title: "Done" },
+    ],
+  },
+  setBoardTask: (payload) => set(() => ({ boardTask: payload })),
+  onBoardingStatus: initRenderOnboardingStatus,
+  setonBoardingStatus: (payload) => set(() => ({ onBoardingStatus: payload })),
+}));
